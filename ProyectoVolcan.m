5b10528491def0e0389a3b3e1f413935476e5c6c
IvanRomero03 <65189646+IvanRomero03@users.noreply.github.com>
2021-10-14 22:54:38 -0700

    Update ProyectoVolcan.m
    Actualización con método de Euler y método algebráico (falta graficar(cambiar de syms a númerico))

--- ProyectoVolcan.m
+++ ProyectoVolcan.m
@@ -1,15 +1,4 @@
-V0 = randi([83, 139],1); %velocidad inicial m/s
-alpha = randi([0, 360],1); %angulo alpha
-beta = randi([30, 150],1); %angulo beta
-gama = randi([0, 360],1); %angulo gama
-x0 = 0; %psoicion inicial x
-y0 = 5492; %posicion inicial y (altura de popocatepetl)
-z0 = 0; %posicion inicial z
-g = 9.8; %gravedad
-V0x = V0*cos(alpha); %Velocidad x
-V0y = V0*cos(beta); %Velocidad y
-V0z = V0*cos(gama); %Velocidad z
-
+%inicializar nuestra cónica
 hold on
 
 r = linspace(0.03*pi,pi) ;
@@ -23,14 +12,103 @@ r = linspace(0.03*pi,pi) ;
     axis([-6000 6000 -6000 6000 0 7000])
     colormap(hot)
 
+%inicializar nuestros valores
+V0 = randi([83, 139],1); %velocidad inicial m/s
+alpha = randi([0, 360],1); %angulo alpha
+beta = randi([30, 150],1); %angulo beta
+gama = randi([0, 360],1); %angulo gama
+x0 = 0; %psoicion inicial x
+y0 = 5492; %posicion inicial y (altura de popocatepetl)
+z0 = 0; %posicion inicial z
+g = 9.8; %gravedad
+V0x = V0*cos(alpha); %Velocidad x
+V0y = V0*cos(beta); %Velocidad y
+V0z = V0*cos(gama); %Velocidad z
+
 hold on;
 
+%Determinar los parámetros iniciales para el *Método de Euler*
 t = 0;
-x = x0 + V0x*t;
-y = y0 + V0y*t - (1/2)*g*t^2;
-z = z0 + V0z*t;
-plot3(x,z,y,'-o');
+intervalo = 1;
+b = 0.5;
+masa = 80;
+pendiente = 0;
 
+%Para y (altura)
+y_velocidad = V0y;
+y_aceleracion = g;
+y_posicion = y0;
+
+%Para x
+x_velocidad = V0x;
+x_posicion = x0;
+
+
+%Para z
+z_velocidad = V0z;
+z_posicion = z0;
+
+plot3(x_posicion,z_posicion,y_posicion,'-o')
+    %establecer las iteraciones por medio del método de euler
+
+while y_posicion(length(y_posicion)) > 0
+    t = t + intervalo;
+    pendiente = -(b * y_velocidad(length(y_velocidad)) / masa) - y_aceleracion;
+    y_velocidad = [y_velocidad; y_velocidad(length(y_velocidad)) + pendiente * intervalo];
+    y_posicion = [y_posicion; y_posicion(length(y_posicion)) + y_velocidad(length(y_velocidad))*intervalo];
+    
+    x_velocidad = [x_velocidad; x_velocidad(length(x_velocidad)) - (b * x_velocidad(length(x_velocidad)) / masa)*intervalo];
+    x_posicion = [x_posicion; x_posicion(length(x_posicion)) - x_velocidad(length(x_velocidad))*intervalo];
+    
+    z_velocidad = [z_velocidad; z_velocidad(length(z_velocidad)) - (b * z_velocidad(length(z_velocidad)) / masa)*intervalo];
+    z_posicion = [z_posicion; z_posicion(length(z_posicion)) - z_velocidad(length(z_velocidad))*intervalo];
+    plot3(x_posicion,z_posicion,y_posicion,'-o')
+    pause(0.1)
+    hold on
+end
+
+
+%modelación con el método analítico
+
+syms y_2(tiempo) x_2(tiempo) z_2(tiempo) vel b m
+
+vel = diff(y_2,tiempo);
+
+% posición y
+eqn_y = diff(y_2,tiempo,2) == -g + b*(vel)/m;
+cond_y = [y_2(0)==y0, vel(0)==V0y];
+ySol(tiempo) = dsolve(eqn_y,cond_y);
+
+vel = diff(x_2,tiempo);
+
+% posición x
+eqn_x = diff(x_2,tiempo,2) == b*(vel)/m;
+cond_x = [x_2(0)==x0, vel(0)==V0x];
+xSol(tiempo) = dsolve(eqn_x,cond_x);
+
+vel = diff(z_2,tiempo);
+
+% posición z
+eqn_z = diff(z_2,tiempo,2) == b*(vel)/m;
+cond_z = [z_2(0)==z0, vel(0)==V0z];
+zSol(tiempo) = dsolve(eqn_z,cond_z);
+tiempo = 0;
+y = y0;
+x = xSol(t);
+z = zSol(t);
+while y(length(y)) > 0
+    tiempo = tiempo + intervalo;
+    y = [y;double(ySol(tiempo))];
+    x = [x;double(xSol(tiempo))];
+    z = [z;double(zSol(tiempo))];
+    plot3(x,z,y,'-o')
+    pause(0.1)
+    hold on
+end
+
+%comenzar con las iteraciones y graficación simultanea
+
+%{
 while y > 0
     t = t + 1;
     x = [x; x0 + V0x*t];
@@ -40,4 +118,6 @@ while y > 0
     pause(0.1)
     hold on
 end
+%}
+
 hold off
